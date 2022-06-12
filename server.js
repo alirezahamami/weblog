49b5810d2a2fa0a5acbd8505e5a26976fc592b04
alirezahamami <alireza.hamami@gmail.com>
2022-06-12 16:42:43 +0430

    body-Parser change to body-parser

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-Parser");
+const bodyParser = require("body-parser");
 app.use(express.static(__dirname + "/views")); //use css for loading
 app.use(bodyParser.urlencoded({
   extended: true
